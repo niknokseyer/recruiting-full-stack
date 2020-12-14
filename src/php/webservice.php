cbc2d6d886db1ab2ac4ee9665ef2ad679c682bd7
Nikko Reyes <admin@senarysoft.com>
2020-12-13 22:22:57 -0800

    Updated PHP to return JSON data.

--- src/php/webservice.php
+++ src/php/webservice.php
@@ -14,4 +14,33 @@ http://localhost:8000/webservice.php
 */
 
 
-echo 'Hello';
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Origin, Content-Type');
+header('Content-Type: application/json');
+
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+
+// retrieve the inbound parameters based on request type.
+if (in_array($requestMethod, ["GET"])) {
+
+    switch ($requestMethod) {
+        case 'GET':
+            $the_request = &$_GET;
+
+            $file = "data.csv";
+            $json = file_get_contents('../data/testdata.json');
+            echo $json;
+            break;
+        default:
+            header("HTTP/1.1 405 Method Not Allowed");
+            $response_data = array("Message" => "Method not allowed.");
+            echo json_encode($response_data);
+    }
+
+} else {
+    header("HTTP/1.1 405 Method Not Allowed");
+    $response_data = array("Message" => "Method not allowed.");
+    echo json_encode($response_data);
+}
