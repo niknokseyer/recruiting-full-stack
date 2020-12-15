0e0b2d37158665135d50ec133a97e056ba8cae6e
Nikko Reyes <admin@senarysoft.com>
2020-12-14 22:04:42 -0800

    Pulling of JSON data and rendering using NodeTree and Collapsible.

--- src/js/App.js
+++ src/js/App.js
@@ -1,13 +1,42 @@
-import React from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
+import NodeTree from './NodeTree'
 
 // NOTE: ESLint will enforce Táve coding standards:
 // https://github.com/tave/javascript/  Goodbye semicolons!
 
 function App() {
+  const [data, setData] = useState({
+    nodes: [],
+    settings: [],
+  })
+  const [hasError, setHasError] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    setIsLoading(true)
+    fetch('http://localhost:8000/webservice.php')
+      .then(response => response.json())
+      .then((json) => {
+        setData(json)
+        setIsLoading(false)
+      })
+      .catch((err) => {
+        setHasError(true)
+        setIsLoading(false)
+        console.log(err)
+      })
+  }, [])
+
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <Fragment>
+      {hasError && <div>Error occurred.</div>}
+
+      {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+        <NodeTree nodes={data.nodes} />
+      )}
+    </Fragment>
   )
 }
 
